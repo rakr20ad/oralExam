7f3d630eb194f9d71b778e4f29af46408d42cc55
rakr20ad <rasmus.woo.kristensen@gmail.com>
2021-04-17 14:07:33 +0200

    beginning of log in

---
+++ Model/users.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(name, email, gender, country, birthday, image){
+        this.name = name; 
+        this.email = email; 
+        this.gender = gender; 
+        this.country = country; 
+        this.birthday = birthday; 
+        this.image = image;
+    }
+}
+
+module.exports = User;
