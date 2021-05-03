dd677122e36f4ae715463f1336167e7d164087c9
loni18ac <70534823+loni18ac@users.noreply.github.com>
2021-05-03 20:33:32 +0200

    algorithmChange

---
+++ services.js
@@ -0,0 +1,36 @@
+const { getUsersNearby } = require("./shared/db");
+const datingUser = require('../oralExam-1/Model/user')
+
+module.exports = {
+    getSuggestions: async (req, res, next) => {
+      var user = await getUsersNearby("id", req.query.id);
+      var city = user[0].city;
+     
+      var list;
+  
+      switch (orientation) {
+        case "hetero":
+          list = await getSuggestions(
+       
+          );
+      }
+  
+      list = await getScoredList(list, user[0]);
+      var idList = [];
+      await list.forEach(element => {
+        idList.push(element.id);
+      });
+  
+      return res.status(200).json({ list });
+    },
+
+getScoredList: async (listData, user) => {
+    var score = listData.copyWithin(0);
+
+    for (var i = 0; i < listData.length; i++) {
+      var count = 0;
+      count +=
+
+    };
+    return score;
+  }
